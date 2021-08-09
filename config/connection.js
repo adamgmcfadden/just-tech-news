3222f3baffe3586623591281e3989970587db9a2
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-08-09 11:15:36 -0400

    completed feature

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+//impot the Squelize constructor from the library
+const Sequelize = require("sequelize");
+
+require("dotenv").config();
+
+//create connection to our database, pass in your MySql information for username and password
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
+
+module.exports = sequelize;
